358c680ca4bcb731d842e5a936529e6e5290e88f
maiky <maiky.williamfs@gmail.com>
2022-10-21 18:54:23 -0300

    Ajuste de pontos

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ const mario =  document.querySelector('.mario');
 const pipe =  document.querySelector('.pipe');
 const over =  document.querySelector('.game-over');
 const btn = document.querySelector(".game-over button");
+let pontuacao = document.querySelector(".pontuacao h1").innerText;
+document.getElementById('pontos').innerText = 'Pontos: '+ 0;
+
+let score = 10;
+let gameover = false;
 
 btn.addEventListener("click", function() {
     
@@ -15,6 +20,16 @@ const jump = () =>{
         mario.classList.remove('jump')
     }, 500);
 }
+
+const scoreFinal = setInterval(() => {
+    
+    if(gameover){
+        return
+    }
+    document.getElementById('pontos').innerText = `Pontos: ${score += 1}`;
+    console.log(score += 1 )
+    score++
+}, 1000);
 
 const loop = setInterval(() =>{
     const pipePosition = pipe.offsetLeft;
@@ -32,9 +47,13 @@ const loop = setInterval(() =>{
         mario.style.marginLeft = '50px'
 
         over.style.display = 'block'
+        gameover = true;
+        
     }
 
+    
 
 }, 10)
 
 document.addEventListener('keydown', jump)
+document.addEventListener("touchstart", jump, false);
